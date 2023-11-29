931c38a619379e453380e78c59c632c7b861f6d1
chen-jin021 <chen_jin@brown.edu>
2023-11-29 18:02:33 -0500

    check secret in init file

--- application/__init__.py
+++ application/__init__.py
@@ -6,7 +6,7 @@ app_path = os.path.dirname(__file__)
 print("app_path: ", app_path)
 app = Flask(__name__, static_folder=app_path+'/static')
 #creating a secret key for security
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba241'
+#app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba241'
 #locating the database in our directory
 #db_path = os.path.join(os.path.dirname(__file__), 'Kanban.db')
 #URI = 'sqlite:///{}'.format(db_path)
