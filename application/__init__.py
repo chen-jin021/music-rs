e96b3ec207fff292f89786e48e7f633c50566b1b
chen-jin021 <chen_jin@brown.edu>
2023-11-29 17:41:12 -0500

    debug heroku log statement

--- application/__init__.py
+++ application/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 
 #create an instance of a flask app and indicating its location
 app_path = os.path.dirname(__file__)
+print("app_path: ", app_path)
 app = Flask(__name__, static_folder=app_path+'/static')
 #creating a secret key for security
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba241'
