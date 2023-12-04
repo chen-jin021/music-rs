b9c1824a460ff69256db1e31e9ac43f738be1cff
chen-jin021 <chen_jin@brown.edu>
2023-12-04 00:45:09 -0500

    update dependencies

--- application/__init__.py
+++ application/__init__.py
@@ -6,7 +6,7 @@ from flask_cors import CORS, cross_origin
 app_path = os.path.dirname(__file__)
 # print("app_path: ", app_path)
 app = Flask(__name__, static_folder=app_path+'/static')
-CORS(app)
+# CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['REMOTE_ADDR'] = '0.0.0.0/0'
 app.secret_key = os.urandom(24) # generate a secret key for the session
